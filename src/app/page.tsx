751c32b579a7af03123e44448bc4c9b3f0f609b0
jisboui <jihedsbouii@gmail.com>
2023-11-11 16:01:26 +0100

    little home page content modification

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div>
-      <h1>Home Page</h1>
-      <p>Some content</p>
+      <h1>Next 13 and pocketbase project</h1>
+      <p>welcome to my next 13 built app</p>
     </div>
   )
 }
